289d4b03324bc7830d1f53c0098c865a5cc8c934
faustinolage <faustino.lage@imatia.com>
2018-11-06 15:32:30 +0100

    Filter cors de primero

--- ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/rest/OntimizeBootCorsFilter.java
+++ ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/rest/OntimizeBootCorsFilter.java
@@ -1,7 +1,11 @@
 package com.ontimize.boot.autoconfigure.rest;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
 import com.ontimize.jee.server.security.cors.OntimizeJeeCorsFilter;
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class OntimizeBootCorsFilter extends OntimizeJeeCorsFilter {
 
 }
