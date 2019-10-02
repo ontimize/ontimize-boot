229bb911b609b2b6073f5166d6e21a3161daad84
Joaquín Romero <joaquin.romero@gmail.com>
2019-10-02 19:09:06 +0200

    mail configuration

--- ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/core/CoreAutoConfiguration.java
+++ ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/core/CoreAutoConfiguration.java
@@ -7,23 +7,29 @@ import org.springframework.context.annotation.Configuration;
 import com.ontimize.jee.server.configuration.OntimizeConfiguration;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import com.ontimize.jee.server.security.SecurityConfiguration;
+import com.ontimize.jee.server.services.mail.MailConfiguration;
 
 @Configuration
 public class CoreAutoConfiguration {
 
-	@Autowired(required=false)
-	SecurityConfiguration securityConfiguration;
-	
+	@Autowired(required = false)
+	SecurityConfiguration	securityConfiguration;
+	@Autowired(required = false)
+	MailConfiguration		mailConfiguration;
+
 	@Bean
-    public DefaultOntimizeDaoHelper defaultOntimizeDaoHelper() {
-        return new DefaultOntimizeDaoHelper();
-    }
-	
+	public DefaultOntimizeDaoHelper defaultOntimizeDaoHelper() {
+		return new DefaultOntimizeDaoHelper();
+	}
+
 	@Bean
 	public OntimizeConfiguration ontimizeConfiguration() {
 		OntimizeConfiguration ontimizeConfiguration = new OntimizeConfiguration();
-		if (securityConfiguration!=null) {
-			ontimizeConfiguration.setSecurityConfiguration(securityConfiguration);
+		if (this.securityConfiguration != null) {
+			ontimizeConfiguration.setSecurityConfiguration(this.securityConfiguration);
+		}
+		if (this.mailConfiguration != null) {
+			ontimizeConfiguration.setMailConfiguration(this.mailConfiguration);
 		}
 		return ontimizeConfiguration;
 	}
---
+++ ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/mail/MailConfig.java
@@ -0,0 +1,234 @@
+package com.ontimize.boot.autoconfigure.mail;
+
+public class MailConfig {
+	private String	encodingKey;
+	private String	hostKey;
+	private String	portKey;
+	private String	protocolKey;
+	private String	userKey;
+	private String	passwordKey;
+	private String	propertiesKey;
+
+	private String	encoding;
+	private String	host;
+	private String	port;
+	private String	protocol;
+	private String	user;
+	private String	password;
+	private String	properties;
+
+	public MailConfig() {
+		super();
+	}
+
+	/**
+	 * @return the encodingKey
+	 */
+	public String getEncodingKey() {
+		return this.encodingKey;
+	}
+
+	/**
+	 * @param encodingKey
+	 *            the encodingKey to set
+	 */
+	public void setEncodingKey(String encodingKey) {
+		this.encodingKey = encodingKey;
+	}
+
+	/**
+	 * @return the hostKey
+	 */
+	public String getHostKey() {
+		return this.hostKey;
+	}
+
+	/**
+	 * @param hostKey
+	 *            the hostKey to set
+	 */
+	public void setHostKey(String hostKey) {
+		this.hostKey = hostKey;
+	}
+
+	/**
+	 * @return the portKey
+	 */
+	public String getPortKey() {
+		return this.portKey;
+	}
+
+	/**
+	 * @param portKey
+	 *            the portKey to set
+	 */
+	public void setPortKey(String portKey) {
+		this.portKey = portKey;
+	}
+
+	/**
+	 * @return the protocolKey
+	 */
+	public String getProtocolKey() {
+		return this.protocolKey;
+	}
+
+	/**
+	 * @param protocolKey
+	 *            the protocolKey to set
+	 */
+	public void setProtocolKey(String protocolKey) {
+		this.protocolKey = protocolKey;
+	}
+
+	/**
+	 * @return the userKey
+	 */
+	public String getUserKey() {
+		return this.userKey;
+	}
+
+	/**
+	 * @param userKey
+	 *            the userKey to set
+	 */
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+
+	/**
+	 * @return the passwordKey
+	 */
+	public String getPasswordKey() {
+		return this.passwordKey;
+	}
+
+	/**
+	 * @param passwordKey
+	 *            the passwordKey to set
+	 */
+	public void setPasswordKey(String passwordKey) {
+		this.passwordKey = passwordKey;
+	}
+
+	/**
+	 * @return the propertiesKey
+	 */
+	public String getPropertiesKey() {
+		return this.propertiesKey;
+	}
+
+	/**
+	 * @param propertiesKey
+	 *            the propertiesKey to set
+	 */
+	public void setPropertiesKey(String propertiesKey) {
+		this.propertiesKey = propertiesKey;
+	}
+
+	/**
+	 * @return the encoding
+	 */
+	public String getEncoding() {
+		return this.encoding;
+	}
+
+	/**
+	 * @param encoding
+	 *            the encoding to set
+	 */
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	/**
+	 * @return the host
+	 */
+	public String getHost() {
+		return this.host;
+	}
+
+	/**
+	 * @param host
+	 *            the host to set
+	 */
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public String getPort() {
+		return this.port;
+	}
+
+	/**
+	 * @param port
+	 *            the port to set
+	 */
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+	/**
+	 * @return the protocol
+	 */
+	public String getProtocol() {
+		return this.protocol;
+	}
+
+	/**
+	 * @param protocol
+	 *            the protocol to set
+	 */
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public String getUser() {
+		return this.user;
+	}
+
+	/**
+	 * @param user
+	 *            the user to set
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return this.password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the properties
+	 */
+	public String getProperties() {
+		return this.properties;
+	}
+
+	/**
+	 * @param properties
+	 *            the properties to set
+	 */
+	public void setProperties(String properties) {
+		this.properties = properties;
+	}
+
+}
