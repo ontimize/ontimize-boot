f2d0ee7799191ce1779a2cde1931cd3f982b3077
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-03-09 15:36:00 +0100

    Added export autoconfigure

--- ontimize-boot-webaddons/src/main/java/com/ontimize/boot/export/excel/ExcelExportAutoConfigure.java
+++ ontimize-boot-webaddons/src/main/java/com/ontimize/boot/export/excel/ExcelExportAutoConfigure.java
@@ -1,8 +1,26 @@
 package com.ontimize.boot.export.excel;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Configuration
-public class ExcelExportAutoConfigure {
+import com.ontimize.jee.webclient.excelexport.base.ExcelExportRestController;
+import com.ontimize.jee.webclient.excelexport.base.ExcelExportService;
 
+@ConditionalOnProperty(name = "ontimize.export.extension", havingValue = "xlsx", matchIfMissing = false)
+@RequestMapping("${ontimize.export.url}")
+public class ExcelExportAutoConfigure extends ExcelExportRestController{
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Bean
+	public ExcelExportService exportService() {
+		return new ExcelExportService();
+	}
 }
