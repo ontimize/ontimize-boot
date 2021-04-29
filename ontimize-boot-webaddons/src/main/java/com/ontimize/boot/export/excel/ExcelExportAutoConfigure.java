d2c53eb05a58018ab860bdc7472ad976b7ea3c00
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-04-29 14:18:50 +0200

    Updated packages and versions

--- ontimize-boot-webaddons/src/main/java/com/ontimize/boot/export/excel/ExcelExportAutoConfigure.java
+++ ontimize-boot-webaddons/src/main/java/com/ontimize/boot/export/excel/ExcelExportAutoConfigure.java
@@ -6,8 +6,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ontimize.jee.webclient.excelexport.base.ExcelExportRestController;
-import com.ontimize.jee.webclient.excelexport.base.ExcelExportService;
+import com.ontimize.jee.webclient.export.base.ExcelExportRestController;
+import com.ontimize.jee.webclient.export.base.ExcelExportService;
 
 @ConditionalOnProperty(name = "ontimize.export.extension", havingValue = "xlsx", matchIfMissing = false)
 @RequestMapping("${ontimize.export.url}")
