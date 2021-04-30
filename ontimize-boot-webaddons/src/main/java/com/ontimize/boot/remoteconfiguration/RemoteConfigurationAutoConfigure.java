70a0c89b8986c2c9929b5a600c6f10b570c9270e
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2021-04-30 13:09:55 +0200

    Added remote configuration autoconfigure

---
+++ ontimize-boot-webaddons/src/main/java/com/ontimize/boot/remoteconfiguration/RemoteConfigurationAutoConfigure.java
@@ -0,0 +1,40 @@
+package com.ontimize.boot.remoteconfiguration;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+import com.ontimize.dto.EntityResult;
+import com.ontimize.jee.webclient.remoteconfiguration.IRemoteConfigurationService;
+
+@Configuration
+@ConditionalOnProperty(name = "ontimize.remote_configuration", havingValue = "true", matchIfMissing = false)
+public class RemoteConfigurationAutoConfigure implements IRemoteConfigurationService{
+	
+	
+	@Override
+	public EntityResult remoteConfigurationQuery(Map<?, ?> keysValues, List<String> attributes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EntityResult remoteConfigurationInsert(Map<?, ?> attributesValues) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EntityResult remoteConfigurationUpdate(Map<?, ?> attributesValues, Map<?, ?> keysValues) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EntityResult remoteConfigurationDelete(Map<?, ?> keysValues) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
