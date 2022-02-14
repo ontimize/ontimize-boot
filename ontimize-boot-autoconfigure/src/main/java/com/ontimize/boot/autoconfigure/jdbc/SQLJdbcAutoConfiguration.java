35f909ba93d938a5c8aa209aeca46280bfc4e309
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2022-02-14 11:11:59 +0100

    Remove conditional on missing bean annotation

--- ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/jdbc/SQLJdbcAutoConfiguration.java
+++ ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/jdbc/SQLJdbcAutoConfiguration.java
@@ -4,7 +4,6 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -98,7 +97,6 @@ public class SQLJdbcAutoConfiguration {
 	}
 
 	@Bean("dbSQLStatementHandler")
-	@ConditionalOnMissingBean
 	public SQLStatementHandler defaultSQLStatementHandler() {
 		SQLStatementHandler handler = new DefaultSQLStatementHandler();
 		handler.setSQLConditionValuesProcessor(this.extendedDefaultSQLConditionValuesProcessor());
