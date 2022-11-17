0c14659824dd891153da09f46e21b305ad6c8011
Xoán Loureiro Santamaría <xoan.loureiro@imatia.com>
2022-11-17 14:17:06 +0100

    Allow transactions using tenants

--- ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/multitenant/MultiTenantAutoConfiguration.java
+++ ontimize-boot-autoconfigure/src/main/java/com/ontimize/boot/autoconfigure/multitenant/MultiTenantAutoConfiguration.java
@@ -2,46 +2,46 @@ package com.ontimize.boot.autoconfigure.multitenant;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 import com.ontimize.jee.server.multitenant.jdbc.IMultiTenantManager;
 import com.ontimize.jee.server.multitenant.jdbc.MultiTenantManager;
 import com.ontimize.jee.server.multitenant.jdbc.MultiTenantRoutingDataSource;
 
 @Configuration
+@ConditionalOnProperty(name = "ontimize.multitenant.enabled", havingValue = "true", matchIfMissing = false)
 public class MultiTenantAutoConfiguration {
 
 	@Bean
-	@ConditionalOnProperty(name = "ontimize.multitenant.enabled", havingValue = "true", matchIfMissing = false)
 	public OntimizeBootMultiTenantFilter tenantFilterChain() {
 		return new OntimizeBootMultiTenantFilter();
 	}
 
 	@Bean(name = "tenantManager")
-	@ConditionalOnProperty(name = "ontimize.multitenant.enabled", havingValue = "true", matchIfMissing = false)
 	@ConditionalOnMissingBean(name = "tenantManager")
 	IMultiTenantManager multiTenantManager() {
 		return new MultiTenantManager();
 	}
 
 	@Bean("tenantDataSource")
-	@ConditionalOnProperty(name = "ontimize.multitenant.enabled", havingValue = "true", matchIfMissing = false)
-	public DataSource multitenantDataSource(DataSource mainDataSource, IMultiTenantManager tenantManager) {
+	public DataSource multitenantDataSource(IMultiTenantManager tenantManager) {
 		MultiTenantRoutingDataSource tenantRoutingDataSource = new MultiTenantRoutingDataSource();
 		tenantRoutingDataSource.setTargetDataSources(tenantManager.getDataSourceHashMap());
-		tenantRoutingDataSource.setDefaultTargetDataSource(mainDataSource);
 
 		tenantManager.setTenantRoutingDataSource(tenantRoutingDataSource);
 
 		return tenantRoutingDataSource;
 	}
 
-	@Bean("tenantDataSource")
-	@ConditionalOnMissingBean(name = "tenantDataSource")
-	public DataSource defaultDataSource(DataSource mainDataSource) {
-		return mainDataSource;
+	@Bean("tenantTransactionManager")
+	public TransactionManager tenantTransactionManager(MultiTenantRoutingDataSource datasource) {
+	    return new JdbcTransactionManager(datasource);
 	}
 }
