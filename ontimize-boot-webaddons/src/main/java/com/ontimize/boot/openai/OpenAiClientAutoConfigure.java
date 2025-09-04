cdc6e133461ad3662407a76285f5bf8fb877942c
adrian-carneiro-imatia <adrian.carneiro@imatia.com>
2025-09-04 09:07:31 +0200

    ammend

--- ontimize-boot-webaddons/src/main/java/com/ontimize/boot/openai/OpenAiClientAutoConfigure.java
+++ ontimize-boot-webaddons/src/main/java/com/ontimize/boot/openai/OpenAiClientAutoConfigure.java
@@ -1,29 +1,20 @@
 package com.ontimize.boot.openai;
 
-import com.ontimize.jee.webclient.openai.client.OpenAiClient;
-import com.ontimize.jee.webclient.openai.model.OpenAiClientConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.ontimize.jee.webclient.openai.client.OpenAiClient;
 
 @Configuration
 @ConditionalOnProperty(name = "ontimize.openai", havingValue = "true", matchIfMissing = false)
 public class OpenAiClientAutoConfigure {
 
     @Value("${ontimize.openai.apiKey}")
-    private String basePath;
-
-    @Value("${ontimize.openai.base-path}")
-    private String basePath;
-
-    @Value("${ontimize.openai.base-path}")
-    private String basePath;
+    private String apiKey;
 
     @Bean("OpenAiClient")
-    public OpenAiClient openAiClient(String apiKey, String model, int maxTokens, double temperature) {
-        return new OpenAiClient(
-                new OpenAiClientConfig(apiKey, model, maxTokens, temperature)
-        );
+    public OpenAiClient openAiClient(String apiKey) {
+        return new OpenAiClient(apiKey);
     }
 }
