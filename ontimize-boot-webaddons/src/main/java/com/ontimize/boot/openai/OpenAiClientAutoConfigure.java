bd16b89cb6b806a39069b8f26b6d90171a3b259c
adrian-carneiro-imatia <adrian.carneiro@imatia.com>
2025-09-15 16:11:56 +0200

    fix autogonfigure

--- ontimize-boot-webaddons/src/main/java/com/ontimize/boot/openai/OpenAiClientAutoConfigure.java
+++ ontimize-boot-webaddons/src/main/java/com/ontimize/boot/openai/OpenAiClientAutoConfigure.java
@@ -1,20 +1,20 @@
 package com.ontimize.boot.openai;
 
+import com.ontimize.jee.webclient.openai.client.OpenAIClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.ontimize.jee.webclient.openai.client.OpenAiClient;
 
 @Configuration
-@ConditionalOnProperty(name = "ontimize.openai", havingValue = "true", matchIfMissing = false)
+@ConditionalOnProperty(name = "ontimize.openai.apikey", matchIfMissing = false)
 public class OpenAiClientAutoConfigure {
 
-    @Value("${ontimize.openai.apiKey}")
+    @Value("${ontimize.openai.apikey}")
     private String apiKey;
 
-    @Bean("OpenAiClient")
-    public OpenAiClient openAiClient(String apiKey) {
-        return new OpenAiClient(apiKey);
+    @Bean("OpenAIClient")
+    public OpenAIClient openAIClient() {
+        return new OpenAIClient(this.apiKey);
     }
 }
